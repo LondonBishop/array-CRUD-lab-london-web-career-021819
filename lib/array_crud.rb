5c4af4173591087496ff4d330f6b3ffe573cb4a0
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-12 18:13:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  return []
 end
 
 def create_an_array
-  
+  return ["BMW", "VW","Audi", "Porsche"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+   return array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  return array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
